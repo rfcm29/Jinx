8b1a59d5d57c0873c2d456618b1639348a8f32be
frabaro <frabaro@hotmail.com>
2022-07-06 00:00:44 +0100

    define cena do jogo

--- src/jinx/PreLobby/PreLobbyController.java
+++ src/jinx/PreLobby/PreLobbyController.java
@@ -5,14 +5,22 @@
  */
 package jinx.PreLobby;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -41,7 +49,15 @@ public class PreLobbyController implements Initializable {
     }    
 
     @FXML
-    private void entrarJogo(ActionEvent event) {
+    private void entrarJogo(ActionEvent event) throws IOException {
+        URL url = new File("src/jinx/Jogo/Jogo.fxml").toURI().toURL();
+        Parent jogo = FXMLLoader.load(url);
+        
+        Scene sceneJogo = new Scene(jogo);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(sceneJogo);
+        window.show();
     }
     
 }
